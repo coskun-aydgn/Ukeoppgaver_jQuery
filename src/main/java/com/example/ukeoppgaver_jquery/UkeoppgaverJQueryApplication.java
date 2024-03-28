33e31f33da7918fe2147205f8e60b57cfb77f018
47462 <coskun.aydn@gmail.com>
2024-03-28 15:37:58 +0100

    new

---
+++ src/main/java/com/example/ukeoppgaver_jquery/UkeoppgaverJQueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgaver_jquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UkeoppgaverJQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UkeoppgaverJQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ukeoppgaver_jquery/UkeoppgaverJQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ukeoppgaver_jquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkeoppgaverJQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
